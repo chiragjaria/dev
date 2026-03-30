1e72152d8784868175068ae640d5d42fc8987f3d
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-03-30 14:41:39 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,10 @@
 
+variable "resource_group" {}
+variable "location" {}
+variable "aks_name" {}
+variable "kv_name" {}
+variable "db_name" {}
+variable "admin_username" {}
+variable "admin_password" {
+  sensitive = true
+}
