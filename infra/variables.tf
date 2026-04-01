5be4a2e171eea41302b8e5483a317edceecab48e
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-04-01 16:53:08 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -8,17 +8,3 @@ variable "admin_username" {}
 variable "admin_password" {
   sensitive = true
 }
-variable "create_aks" {
-  type    = bool
-  default = false
-}
-
-variable "create_akv" {
-  type    = bool
-  default = false
-}
-
-variable "create_db" {
-  type    = bool
-  default = false
-}
