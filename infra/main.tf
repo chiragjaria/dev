52a668f1531de1ef427f5b1c1f2f02b86dde7455
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-04-01 17:08:53 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -14,22 +14,23 @@ provider "azurerm" {
 
 # Resource Group
 data "azurerm_resource_group" "rg" {
-  name = var.resource_group
+  name     = var.resource_group
 }
 
 # Key Vault
-
+resource "azurerm_key_vault" "kv" {
   name                        = var.kv_name
-  location                    = data.azurerm_resource_group.rg.location
-  resource_group_name         = data.azurerm_resource_group.rg.name
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
   tenant_id                   = "f9619075-6411-4bf8-8c43-c9b10b59452b"
   sku_name                    = "standard"
+}
 
 # AKS
-
+resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_name
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "aksdns"
 
   default_node_pool {
@@ -41,11 +42,13 @@ data "azurerm_resource_group" "rg" {
   identity {
     type = "SystemAssigned"
   }
+}
 
 # PostgreSQL Flexible Server
+resource "azurerm_postgresql_flexible_server" "db" {
   name                = var.db_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
   administrator_login    = var.admin_username
   administrator_password = var.admin_password
@@ -54,3 +57,4 @@ data "azurerm_resource_group" "rg" {
   storage_mb = 32768
   version    = "14"
   public_network_access_enabled = false
+}
